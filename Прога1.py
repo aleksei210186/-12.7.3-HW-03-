0c9765f5ab6488add8082ec362d61e4a9da4b376
Алексей <113529635+aleksei210186@users.noreply.github.com>
2022-10-18 15:05:37 +0200

    Delete Прога1.py

--- Прога1.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Введите сумму вклада: '))
-print ("maney =", a)
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-b = 100
-list = [int((a*per_cent['ТКБ'])/b), int((a*per_cent['СКБ'])/b), int((a*per_cent['ВТБ'])/b), int((a*per_cent['СБЕР'])/b)]
-print ("deposit =", list)
-print ("Максимальная сумма, которую вы можете заработать —", max(list))
